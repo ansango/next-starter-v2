85d1e3bf2622e11879d3382680c0f28842691937
ansango <anibalsantosgo@gmail.com>
2021-09-29 21:44:03 +0200

    preact

--- next.config.js
+++ next.config.js
@@ -13,18 +13,17 @@ module.exports = {
     ];
   },
   i18n,
-  // webpack: (config, { dev, isServer }) => {
-  //   // Replace React with Preact only in client production build
-  //   if (!dev && !isServer) {
-  //     Object.assign(config.resolve.alias, {
-  //       react: "preact/compat",
-  //       "react-dom/test-utils": "preact/test-utils",
-  //       "react-dom": "preact/compat",
-  //     });
-  //   }
-
-  //   return config;
-  // },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+    return config;
+  },
 };
 
 // https://securityheaders.com
